5907a1916b48bbd31778259952ead28a20c326e8
moncheck-e <moncheck.e@northeastern.edu>
2025-01-11 12:22:36 -0500

    person class written

--- hw01.cpp
+++ hw01.cpp
@@ -19,25 +19,41 @@ class Person
         //fills the thee private Person variables with user inputs
         void readPersonInfo()
         {
+            cout << "Enter the name of the person: " ;
+            cin >> fullname ;
+            cout << endl << "Enter the address of the person: ";
+            cin >> address;
+            cout << endl << "Enter the age of the person: ";
+            cin >> age;
+
             //age verified to be between 9 and 99
+            while(age < 9 || age > 99)
+            {
+                cout << endl << "Invalid age. Please enter an age between 9 and 99.";
+                cin >> age;
+            }
+
+            cout << endl << "Person attributes assigned successfully.";
         }
 
         //prints the three private Person variables
         void printPersonInfo()
         {
-
+            cout << "name: " << fullname << endl;
+            cout << "address: " << address << endl;
+            cout << "age: " << age << endl;
         }
 
         //returns person's age
         int personAGE()
         {
-
+            return age;
         }
 
         //returns person's name
         string personName()
         {
-
+            return fullname;
         }
 };
 
@@ -85,55 +101,70 @@ class Course
 
     public:
 
-        //
+        //displays the above Course’s three private attributes
         void printCourse()
         {
 
         }
 
-        //add constructor
+        //add a constructor that is used to fill the data of the Course’s three private attributes.
+        Course()
+        {
+            
+        }
 
 };
 
 class CourseSection : public Course
 {
     private:
+        //Number of students enrolled in the section
         int sectionSize;
+        //to store a list of Student objects representing the students enrolled in the section
         Student *roster;
 
     public:
 
-        //
+        // reads the info of all students in the section one at a time,
+        // using readStudentInfo(). After reading the info of a student object, insert it
+        // in the roster list
         void fillRosterInfo()
         {
 
         }
 
-        //
+        //prints the section info starting with its course info,
+        //followed by printing the info of all students in the section
         void printSectionInfo()
         {
 
         }
 
-        //
+        //prints the average GPA of the students in the section
         void printAverageGPA()
         {
 
         }
 
-        //
+        //prints the max GPA and the student info with that GPA
+        //Assume there is no more than one student with such max GPA.
         void printMaxGPAinfo()
         {
 
         }
 
-        //
+        //prints info of the students in the
+        //section who are from major mjr
         void printMajorStudents(string mjr)
         {
 
         }
 
-        //add courseSection constructor and destructor
+        ///a constructor that takes as argument the three attributes of the Course to which the
+        //section belongs as well as the section size. First, this constructor invokes the base
+        //class (Course) constructor using the course three attributes. Then it dynamically
+        //creates the array needed for the roster attribute with size sectionSize.
+        /// a destructor to delete any dynamically allocated memory
 };
 
 int main() {
